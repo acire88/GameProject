d7b15744a3ebd432ddc4017ad7bead05c856c07f
Erica <younge1027@gmail.com>
2019-07-24 11:28:18 -0500

    first version

---
+++ src/test/java/com/company/GameProject/GameProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.GameProject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
